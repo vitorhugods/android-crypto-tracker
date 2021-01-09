dffc924fbb7bd1ab8b216f337c0d5a5eaf9a9c23
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-01-09 19:37:33 -0300

    Remove Annotation classes that were not used

--- ticker/src/release/java/xyz/schwaab/crypto/util/OpenAnnotations.kt
+++
@@ -1,7 +0,0 @@
-package xyz.schwaab.crypto.util
-
-/**
- * Annotate a class with [OpenForTesting] if you want it to be extendable in debug builds.
- */
-@Target(AnnotationTarget.CLASS)
-annotation class OpenForTesting
